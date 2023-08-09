8b3815084b5fc241b5b54dd03d414b70862af164
KG <kimkiwonn@gmail.com>
2023-08-10 02:02:36 +0900

    Add Application.kt and fix BridgeGame for run

--- src/main/kotlin/bridge/Application.kt
+++ src/main/kotlin/bridge/Application.kt
@@ -1,5 +1,44 @@
 package bridge
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("다리 건너기 게임을 시작합니다.")
+    println()
+    println("다리의 길이를 입력해주세요.")
+    val inputView = InputView()
+    val outputView = OutputView()
+    val bridgeSize = inputView.readBridgeSize(Console.readLine())
+
+    val bridgeRandomNumberGenerator = BridgeRandomNumberGenerator()
+    val bridgeMaker = BridgeMaker(bridgeRandomNumberGenerator)
+    val answerBridge = bridgeMaker.makeBridge(bridgeSize)
+
+    val bridgeGame = BridgeGame(answerBridge)
+    var isQuit = false
+
+    while(!bridgeGame.isFinish() && !isQuit) {
+        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+        val moving = inputView.readMoving(Console.readLine())
+
+        val isMove = bridgeGame.move(moving)
+        outputView.printMap(bridgeGame.getUserBridgeMap())
+
+        if (!isMove) {
+            println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+            val gameCommand = inputView.readGameCommand(Console.readLine())
+            if (gameCommand == "R") {
+                bridgeGame.retry()
+            } else {
+                isQuit = true
+            }
+        }
+    }
+
+    println()
+    println("최종 게임 결과")
+    outputView.printMap(bridgeGame.getUserBridgeMap())
+
+    println()
+    outputView.printResult(bridgeGame.isFinish(), bridgeGame.tryCount)
 }
--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -6,7 +6,8 @@ package bridge
 class BridgeGame(private val answerBridge: List<String>) {
     val userBridge = mutableListOf<String>()
     val moveSuccess = mutableListOf<Boolean>()
-    var tryCount = 0
+    var userLocate = 0
+    var tryCount = 1
 
     /**
      * 사용자가 칸을 이동할 때 사용하는 메서드
@@ -14,11 +15,13 @@ class BridgeGame(private val answerBridge: List<String>) {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move(index: Int, moving: String): Boolean {
-        val canMove = (answerBridge[index] == moving)
+    fun move(moving: String): Boolean {
+        val canMove = (answerBridge[userLocate] == moving)
 
         userBridge.add(moving)
         moveSuccess.add(canMove)
+
+        if (canMove) userLocate++
 
         return canMove
     }
@@ -32,32 +35,35 @@ class BridgeGame(private val answerBridge: List<String>) {
     fun retry() {
         userBridge.clear()
         moveSuccess.clear()
+        userLocate = 0
         tryCount++
     }
 
     fun getUserBridgeMap(): String{
-        val sb = StringBuilder()
+        val upSb = StringBuilder()
+        val downSb = StringBuilder()
 
         for (i in 0 until userBridge.size) {
-            sb.append("""
-                    [ ${getUpMark(userBridge[i], moveSuccess[i])} 
-                    [ ${getDownMark(userBridge[i], moveSuccess[i])} 
-                """.trimIndent())
+            upSb.append("[ ${getUpMark(userBridge[i], moveSuccess[i])} ")
+            downSb.append("[ ${getDownMark(userBridge[i], moveSuccess[i])} ")
 
             if (i == userBridge.size - 1) {
-                sb.append("""
-                    ]
-                    ]
-                """.trimIndent())
+                upSb.append("]")
+                downSb.append("]")
             } else {
-                sb.append("""
-                    |
-                    |
-                """.trimIndent())
+                upSb.append("| ")
+                downSb.append("| ")
             }
         }
 
-        return sb.toString()
+        return """
+            $upSb
+            $downSb
+        """.trimIndent()
+    }
+
+    fun isFinish(): Boolean {
+        return moveSuccess.size == answerBridge.size && moveSuccess.all { it == true }
     }
 
     private fun getUpMark(userPick: String, moveSuccess: Boolean): String {
